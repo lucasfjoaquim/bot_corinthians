b1a9341b69b04275c4737da2fdd9dafadbae0eeb
lucasfjoaquim <45808571+lucasfjoaquim@users.noreply.github.com>
2023-07-11 20:24:01 -0300

    primeiro commit

---
+++ help_cog.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+
+class help_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.help_message = "teste"
+        """
+```
+General commands:
+/help - displays all the available commands
+/p <keywords> - finds the song on youtube and plays it in your current channel. Will resume playing the current song if it was paused
+/q - displays the current music queue
+/skip - skips the current song being played
+/clear - Stops the music and clears the queue
+/leave - Disconnected the bot from the voice channel
+/pause - pauses the current song being played or resumes if already paused
+/resume - resumes playing the current song
+```
+"""
+        self.text_channel_list = []
+
+    #some debug info so that we know the bot has started    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("on_ready")
+        for guild in self.bot.guilds:
+            print(guild)
+            for channel in guild.text_channels:
+                if str(channel).strip() == "teste":
+                    print(channel)
+                    self.text_channel_list.append(channel)
+
+        await self.send_to_all(self.help_message)        
+
+    @commands.command(name="help", help="Displays all the available commands")
+    async def help(self, ctx):
+        print("help")
+        await ctx.send(self.help_message)
+
+    async def send_to_all(self, msg):
+        print("send to all")
+        for text_channel in self.text_channel_list:
+            await text_channel.send(msg)
---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+
+#import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+bot.add_cog(help_cog(bot))
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+bot.run("MTA3NjIxODM4NDUwMjQ5MzI1NA.Gk5VxM.KPvS_ZRG2CuX73G0B-vOTgmcI7Oyn0T2FwPFOA")
